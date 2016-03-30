950cd7b96d89f48d9e56734c7bf1f5d994b5b06e
Keiichiro Ono <keiono@gmail.com>
2016-03-30 11:22:30 -0700

    Preview template will be replaced when version number does not match.

--- src/main/java/org/cytoscape/io/internal/preview/PreviewTemplateGenerator.java
+++ src/main/java/org/cytoscape/io/internal/preview/PreviewTemplateGenerator.java
@@ -5,14 +5,23 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
 import org.cytoscape.application.CyApplicationConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class PreviewTemplateGenerator {
+	
+	private final static Logger logger = LoggerFactory.getLogger(PreviewTemplateGenerator.class);
 
+	private static final String VERSION = "0.1.5";
 	private static final String TEMPLATE_NAME = "template.zip";
+	private static final String VERSION_NAME = "version.txt";
 	public static final String WEB_RESOURCE_DIR_NAME = "preview_template";
 
 	private final CyApplicationConfiguration appConfig;
@@ -31,7 +40,38 @@ public class PreviewTemplateGenerator {
 		// Unzip resource to this directory in CytoscapeConfig
 		if (!destination.exists() || !destination.isDirectory()) {
 			unzipTemplate(source, destination);
+		} else if(destination.exists()) {
+			// Maybe there is an old version
+			final File versionFile = new File(destination, VERSION_NAME);
+			if(!versionFile.exists()) {
+				logger.info("Version file not found.  Creating new preview template...");
+				deleteAll(destination);
+				unzipTemplate(source, destination);
+			} else {
+				// Check version number
+				final String contents = Files.lines(Paths.get(versionFile.toURI()))
+					.reduce((t, u) -> t+u).get();
+				
+				logger.info("Preview template version: " + contents);
+				logger.info("Current template version: " + VERSION);
+				
+				if(!contents.equals(VERSION)) {
+					logger.info("Updating template to version " + VERSION);
+					deleteAll(destination);
+					unzipTemplate(source, destination);
+				} else {
+					logger.info("No need to update preview template.");
+				}
+			}
 		}
+	}
+	
+	private final void deleteAll(final File f) {
+		if(f.isDirectory()) {
+			final File[] files = f.listFiles();
+			Arrays.stream(files).forEach(file->deleteAll(file));
+		}
+		f.delete();
 	}
 
 	public void unzipTemplate(final URL source, final File destDir) throws IOException {
